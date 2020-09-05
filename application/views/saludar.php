fad154934a23621f6b156bc2ace02063b09ca3e3
yessica324 <yessiquita123quispe@gmail.com>
2020-09-05 17:34:39 -0400

    Accesos

--- application/views/saludar.php
+++ application/views/saludar.php
@@ -2,5 +2,8 @@
 
 
 
+<img src="<?php echo base_url(); ?>img/calaquita.png">
+
+
 <!-- http://localhost/web8/index.php/welcome/saludar -->
 <a href="<?php echo base_url();?>welcome/despedir">IR A DESPEDIR</a>
